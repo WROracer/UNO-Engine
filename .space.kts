5ff3069904692ff1354531a868237d2b10e3b635
WROracer <67609673+WROracer@users.noreply.github.com>
2022-06-26 22:22:34 +0200

    init

---
+++ .space.kts
@@ -0,0 +1,18 @@
+job("Build, run tests, publish") {
+    container(displayName = "Run publish script", image = "maven:3-openjdk-8-slim") {
+        // url of a Space Packages repository
+        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
+
+        shellScript {
+            content = """
+                echo Build and publish artifacts...
+                set -e -x -u
+                mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
+                mvn deploy -s settings.xml \
+                    -DrepositoryUrl=${'$'}REPOSITORY_URL \
+                    -DspaceUsername=${'$'}JB_SPACE_CLIENT_ID \
+                    -DspacePassword=${'$'}JB_SPACE_CLIENT_SECRET
+            """
+        }
+    }
+}
