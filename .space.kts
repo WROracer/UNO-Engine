5d81a48f56645fbae849803163b1131ccba6947c
WROracer <67609673+WROracer@users.noreply.github.com>
2022-06-26 23:00:11 +0200

    passe automation an

--- .space.kts
+++ .space.kts
@@ -1,4 +1,32 @@
 job("Build, run tests, publish") {
+    container("run build script","maven:3-openjdk-8-slim") {
+
+        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
+
+        shellScript {
+            content = """
+                echo Build artifacts...
+                set -e -x -u
+                mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
+                mvn package
+            """
+        }
+    }
+
+    container("run test script","maven:3-openjdk-8-slim") {
+
+        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
+
+        shellScript {
+            content = """
+                echo Build artifacts...
+                set -e -x -u
+                mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
+                mvn test
+            """
+        }
+    }
+
     container(displayName = "Run publish script", image = "maven:3-openjdk-8-slim") {
         // url of a Space Packages repository
         env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
@@ -8,7 +36,6 @@ job("Build, run tests, publish") {
                 echo Build and publish artifacts...
                 set -e -x -u
                 mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
-                mvn test
                 mvn deploy -s settings.xml \
                     -DrepositoryUrl=${'$'}REPOSITORY_URL \
                     -DspaceUsername=${'$'}JB_SPACE_CLIENT_ID \
