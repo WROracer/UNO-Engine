9e29d4825cbe5d2004d7131e72fce56598446d96
WROracer <67609673+WROracer@users.noreply.github.com>
2022-06-26 23:04:17 +0200

    fuege test hinzuh

--- .space.kts
+++ .space.kts
@@ -1,38 +1,25 @@
 job("Build, run tests, publish") {
-    container("run build script","maven:3-openjdk-8-slim") {
+    parallel {
+        container("run test script", "maven:3-openjdk-8-slim") {
 
-        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
+            env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
 
-        shellScript {
-            content = """
-                echo Build artifacts...
-                set -e -x -u
-                mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
-                mvn package
-            """
-        }
-    }
-
-    container("run test script","maven:3-openjdk-8-slim") {
-
-        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
-
-        shellScript {
-            content = """
+            shellScript {
+                content = """
                 echo Build artifacts...
                 set -e -x -u
                 mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
                 mvn test
             """
+            }
         }
-    }
 
-    container(displayName = "Run publish script", image = "maven:3-openjdk-8-slim") {
-        // url of a Space Packages repository
-        env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
+        container(displayName = "Run publish script", image = "maven:3-openjdk-8-slim") {
+            // url of a Space Packages repository
+            env["REPOSITORY_URL"] = "https://maven.pkg.jetbrains.space/mycompany/p/key/my-maven-repo"
 
-        shellScript {
-            content = """
+            shellScript {
+                content = """
                 echo Build and publish artifacts...
                 set -e -x -u
                 mvn versions:set -DnewVersion=1.0.${'$'}JB_SPACE_EXECUTION_NUMBER
@@ -41,6 +28,7 @@ job("Build, run tests, publish") {
                     -DspaceUsername=${'$'}JB_SPACE_CLIENT_ID \
                     -DspacePassword=${'$'}JB_SPACE_CLIENT_TOKEN
             """
+            }
         }
     }
 }
